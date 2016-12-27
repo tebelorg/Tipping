0098de293668dc4d596a8ba26d45e8befaf4c096
Ken Soh <ken@tebel.sg>
2016-12-27 16:34:29 +0800

    move to src folder

---
+++ src/mailer.php
@@ -0,0 +1,71 @@
+<?php
+
+/* MAILER REST API FOR TA.MAIL TO AUTO-SEND EMAILS ~ TEBEL.SG */
+
+// address email recipient by name if recipient name provided
+if ($_GET['SENDNAME']=="")
+	$name = "";
+else
+	$name = "<p>Hi " . $_GET['SENDNAME'] . "," . "</p><p></p>";
+
+// your catch-all email in case recipient email not provided
+if ($_GET['SENDTO']=="")
+	$to = "your_email@gmail.com";
+else
+	$to = $_GET['SENDTO'];
+
+// your default send from email if send from email not provided
+if ($_GET['SENDFROM']=="")
+        $from = "Your Name <your_email@gmail.com>";
+else
+        $from = $_GET['SENDFROM'];
+
+// set default email subject if email subject not provided
+if ($_GET['SUBJECT']=="")
+	$subject = "Email Service";
+else
+	$subject = $_GET['SUBJECT'];
+
+// set message body to blank if email message not provided
+if ($_GET['MESSAGE']=="")
+	{$custom_message = "Message body is empty."; $message = "";}
+else
+	{
+// set your email footer below to be used when there is message body
+$custom_message = $_GET['MESSAGE'];
+$message = "
+<html>
+<head>
+<title>" . $subject . "</title>
+</head>
+<body>
+" . $name . "
+<p>" . $custom_message . "<br>" .
+"<p></p>
+<pre><span style=\"font-family: arial, helvetica, sans-serif; font-size: small;\">Kind Regards,<br><span style=\"color: #000000;\"><strong><em>Your Name</em></strong></span></span></pre></p>
+</body>
+</html>";
+	}
+
+$headers  = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: ' . $from . "\r\n";
+
+// customise result output below to show email success or failure
+// first block is to show output as raw text, second block as html
+if ($_GET['OUTPUT']=="TEXT")
+        {
+        if (mail($to,$subject,$message,$headers))
+                echo $subject . " mail sent successfully.\n";
+        else
+                echo $subject . " mail not sent through.\n";
+        }
+else
+	{
+        if (mail($to,$subject,$message,$headers))
+        	echo "<h1><center><br><br><br><br><br><br><br><br>".$subject." mail sent successfully.</center></h1>";
+        else
+        	echo "<h1><center><br><br><br><br><br><br><br><br>".$subject." mail not sent through.</center></h1>";
+	}
+
+?>
---
+++ src/pinger.php
@@ -0,0 +1,53 @@
+<?php
+
+/* TA.PING SCRIPT TO CHECK WEBSITE OR WEBSERVICE AVAILABILITY ~ TEBEL.SG */
+
+// set what site to check and what string to check for
+$URL = $argv[1]; $CHECK = $argv[2];
+
+// alternative usage to call directly from web url instead
+// $URL = $_GET['URL']; $CHECK = $_GET['CHECK'];
+
+// call helper to fetch website in html and without cache
+$content = get_data($URL); // echo $content . "\n"; // for debugging
+
+// check for presence of string on webpage
+if (strpos($content, $CHECK) !== false)
+	echo date('Y-m-d H:i:s') . " - PASS\n"; // show timestamp
+else
+{
+	echo date('Y-m-d H:i:s') . " - FAIL\n"; // show timestamp
+	$_GET['OUTPUT'] = "TEXT"; // set mail result output as text
+	$_GET['SENDTO'] = "your_email@gmail.com"; // set to alert recipient
+	$_GET['SENDFROM'] = "Your Name <your_email@gmail.com>"; // set from
+	$_GET['SUBJECT'] = $URL . " down"; // set subject for email alert
+	// set message in email alert below or leave commented for no email body
+	// $_GET['MESSAGE'] = "Cannot find '" . $CHECK . "' at " . $URL . " website.";
+	sendmail_service();
+}
+
+/* SENDMAIL SERVICE */
+function sendmail_service() { // call mailer REST API to send email
+	ob_start(); include('/full_path_on_your_server/mailer.php');
+	$php_result = ob_get_contents(); ob_end_clean(); echo $php_result;
+}
+
+/* GET DATA FROM URL */
+function get_data($url) {
+	$ch = curl_init();
+	$timeout = 60;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+	$headers = array("Cache-Control: no-cache","Pragma: no-cache");	
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+	curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
+
+?>
